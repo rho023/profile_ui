3267195849ee45b8cbc9a3aaa112fd2ea53ad59e
rohan09lac <rohanlakhera023@gmail.com>
2025-05-19 23:33:23 +0530

    Profile activity completed

--- app/src/main/java/com/example/profileui/presentation/Profile.kt
+++ app/src/main/java/com/example/profileui/presentation/Profile.kt
@@ -1,17 +1,18 @@
-package com.example.profileui
+package com.example.profileui.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.profileui.R
 
-class MainActivity : AppCompatActivity() {
+class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
